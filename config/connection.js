54d5931de357dd50b7fb56d88ed8cb4b12238670
VishwaJarsaniya <vishwa.jarsaniya@gmail.com>
2024-09-27 21:13:20 +0530

    connection fixed

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 
-async function connectMongoDb(url){
-    return await mongoose.connect(url,  {
-        serverSelectionTimeoutMS: 20000, // Increase timeout to 20 seconds
-        socketTimeoutMS: 45000})
-                         .then(()=>{console.log("MongoDb Connected")})
-                         .catch((err)=>{console.log("Error in connecting db", err)})
+async function connectMongoDb(url) {
+    // URL encode the MongoDB connection string
+    const encodedUrl = encodeURI(url);
+
+    return await mongoose.connect(encodedUrl)
+        .then(() => {
+            console.log("MongoDb Connected");
+        })
+        .catch((err) => {
+            console.log("Error in connecting db", err);
+        });
 }
 
 module.exports = {
     connectMongoDb,
-}
+};
