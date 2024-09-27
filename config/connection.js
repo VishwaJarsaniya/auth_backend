aecd0d16d596ee3405998398419e413c074ce72e
VishwaJarsaniya <vishwa.jarsaniya@gmail.com>
2024-09-27 20:40:10 +0530

    timeout

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 async function connectMongoDb(url){
-    return await mongoose.connect(url)
+    return await mongoose.connect(url,  {
+        serverSelectionTimeoutMS: 20000, // Increase timeout to 20 seconds
+        socketTimeoutMS: 45000})
                          .then(()=>{console.log("MongoDb Connected")})
                          .catch((err)=>{console.log("Error in connecting db", err)})
 }
