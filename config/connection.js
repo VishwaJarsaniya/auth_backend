41718ab83c0c01e3803934f3fe29aa95b9f0e4ed
VishwaJarsaniya <vishwa.jarsaniya@gmail.com>
2024-09-28 11:06:51 +0530

    connection changed

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,9 @@
 const mongoose = require("mongoose");
-
-async function connectMongoDb(url) {
-    // URL encode the MongoDB connection string
-    const encodedUrl = encodeURI(url);
-
-    return await mongoose.connect(encodedUrl)
-        .then(() => {
-            console.log("MongoDb Connected");
-        })
-        .catch((err) => {
-            console.log("Error in connecting db", err);
-        });
+async function connectMongoDb(url){
+    return await mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true })
+                         .then(()=>{console.log("MongoDb Connected")})
+                         .catch((err)=>{console.log("Error in connecting db", err)})
 }
-
 module.exports = {
     connectMongoDb,
-};
+}
