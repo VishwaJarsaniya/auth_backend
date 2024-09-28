c61e33858f9cc6c99267586440af846ef7d4197e
VishwaJarsaniya <vishwa.jarsaniya@gmail.com>
2024-09-28 11:12:24 +0530

    minor change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const {connectMongoDb} = require("./config/connection");
 const userRoute = require("./routes/user");
 
